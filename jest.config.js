4d3a833edc12fd583384d45b85ff91233542f47a
gnrfilipe04 <gnr_filipe@hotmail.com>
2022-12-14 21:58:50 -0300

    :sparkles: add jest library

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'jest-expo',
+  testPathIgnorePatterns: [
+    '/node_modules',
+  ],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-native/extend-expect',
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.tsx',
+    '!src/**/*.spec.tsx',
+  ],
+}
