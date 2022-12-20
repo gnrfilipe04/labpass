0951569cc569823f3ed2e1479ee6fb37619df65a
gnrfilipe04 <gnr_filipe@hotmail.com>
2022-12-20 15:09:33 -0300

    :hammer: add space on title button

--- src/components/MyButton/index.tsx
+++ src/components/MyButton/index.tsx
@@ -31,7 +31,7 @@ export function MyButton({
     <Pressable _pressed={{ opacity: 0.8, }} opacity={disable ? 0.5 : 1} onPress={() => !disable && onPress()} bg={bgColor || 'secondary.500'} borderRadius={'full'} p='10px'>
       <HStack justifyContent={'center'} alignItems='center'>
         { leftIcon && <Icon as={leftIcon} color={leftIconColor} size={iconSize} position='absolute' left={0}/> }
-        <Text textAlign={'center'} color={textColor} fontFamily={'Inter_400Regular'}>{title}</Text>
+        <Text paddingY={'5px'}  textAlign={'center'} color={textColor} fontFamily={'Inter_400Regular'}>{title}</Text>
         { rightIcon && <Icon as={rightIcon} color={rightIconColor} size={iconSize} position='absolute' right={0}/> }
       </HStack>
     </Pressable>
